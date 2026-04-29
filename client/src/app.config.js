95c6973b5a8cadd8e8caee898883047bd7b8a3d6
raedfelfel1 <raed.felfel@univ-reims.fr>
2026-04-29 11:14:08 +0200

    fix : resolve email + API issues for production

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,13 +1,13 @@
-/*export default {
+export default {
   extra: {
     API_IP: "https://medagricycle-1.onrender.com/api"
   }
-};*/
-export default {
+};
+/*export default {
   extra: {
    API_IP: "https://anabella-fulminatory-skiagraphically.ngrok-free.dev/api"
   }
-};
+};*/
 
 // 10.251.40.177 
 // http://10.0.2.2:5000/api
--- client/src/contexts/utils/env.js
+++ client/src/contexts/utils/env.js
@@ -1,2 +1,2 @@
-//export const API_IP = "https://medagricycle-1.onrender.com/api";
-export const API_IP = "http://localhost:5000"+"/api"
+export const API_IP = "https://medagricycle-1.onrender.com/api";
+//export const API_IP = "http://localhost:5000"+"/api"
